82bbac17ef3b556ec8d80ba80f04d56455117280
lyna <omarilyna33@gmail.com>
2026-03-24 15:26:32 +0100

    LegalController.php
    Added LegalController to display legal mentions and site information.

--- app/Controllers/LegalController.php
+++ app/Controllers/LegalController.php
@@ -1,1 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+// Ce controleur affiche les mentions legales et les informations obligatoires du site.
+
+namespace App\Controllers;
+
+use Core\View;
+
+class LegalController
+{
+    public function mentions(): void
+    {
+        View::render('legal/mentions', [
+            'title' => 'Web4Stage - Mentions légales',
+        ]);
+    }
+}
+
 
